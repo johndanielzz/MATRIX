2145e6a091bdd3fcad2874c12f1c21dbab24577f
johndanielzz <jd0447754@gmail.com>
2025-10-01 03:50:34 +0100

    Delete matrix-social/client directory

--- matrix-social/client/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Welcome to Matrix 🚀</h1>
-    </div>
-  );
-}
